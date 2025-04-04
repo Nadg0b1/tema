31e7650b500d54a3421f899433635a3d740a2d19
Nadg0b1 <bogditheking@gmail.com>
2025-04-04 23:37:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1
+let x = 9;
+if (x >= 0) {
+  console.log("The number is positive.");
+} else {
+  console.log("The number is negative.");
+}
+
+// 2
+let obtainedGrade = 5;
+if (obtainedGrade < 5) {
+  console.log("Picat");
+} else {
+  console.log("Promovat");
+}
+
+// 3
+let username = "Nadg0b";
+if (username.lenght > 50) {
+  console.log("Too long");
+} else {
+  console.log("Good");
+}
